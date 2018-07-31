456a62ea991bc4d0103026a8928f9b0b89896dec
Esten Rye <esten@ryezone.net>
2018-07-30 21:03:36 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# rz_docker_wrap
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: rz_docker_wrap
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
