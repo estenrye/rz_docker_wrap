50744529d1b5ece2bbaf5a16366ca6f8905b4e13
Esten Rye <esten@ryezone.net>
2018-08-02 23:37:01 -0500

    Adds initial docker configuration.
    Enables prometheus endpoint and experimental features in daemon.json.
    Installs docker.
    Configures docker in swarm mode.

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,27 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+template '/etc/docker/daemon.json' do
+  source 'daemon.json.erb'
+  owner 'root'
+  mode '0644'
+  variables(
+    metrics_addr: '127.0.0.1:9323',
+    experimental: true
+  )
+  action :create
+  notifies :restart, 'service[docker]', :delayed
+end
+
+docker_installation_package 'default' do
+  action :create
+  package_options %q|--force-yes -o Dpkg::Options::='--force-confold' -o Dpkg::Options::='--force-all'| # if Ubuntu for example
+end
+
+execute 'Initialize Swarm' do
+  command 'docker swarm init'
+  guard_interpreter :bash
+  only_if 'if [ "$(sudo docker info --format {{.Swarm.LocalNodeState}})" = "inactive" ]; then exit 0; else exit 1; fi'
+end
+
