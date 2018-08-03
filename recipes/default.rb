f5ba836caf8cb4855b306e2e72f2c293a1f56d17
Esten Rye <esten@ryezone.net>
2018-08-03 01:00:56 -0500

    Add prometheus config and stack file templates.

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,11 +9,33 @@ template '/etc/docker/daemon.json' do
   owner 'root'
   mode '0644'
   variables(
-    metrics_addr: '127.0.0.1:9323',
+    metrics_addr: '0.0.0.0:9323',
     experimental: true
   )
   action :create
   notifies :restart, 'service[docker]', :delayed
+end
+
+directory '/opt/prometheus' do
+  owner 'root'
+  mode '0755'
+  recursive true
+  action :create
+end
+
+template '/opt/prometheus/promethus.yml' do
+  source 'prometheus.yml.erb'
+  owner 'root'
+  mode '0644'
+  variables(
+    docker_ip: node['ipaddress']
+  )
+end
+
+template '/opt/prometheus/promethus.stack.yml' do
+  source 'prometheus.stack.yml.erb'
+  owner 'root'
+  mode '0644'
 end
 
 docker_installation_package 'default' do
@@ -27,3 +49,6 @@ execute 'Initialize Swarm' do
   only_if 'if [ "$(sudo docker info --format {{.Swarm.LocalNodeState}})" = "inactive" ]; then exit 0; else exit 1; fi'
 end
 
+service 'docker' do
+  action :enable
+end
